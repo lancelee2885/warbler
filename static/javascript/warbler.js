ca6127cfa75d71f38b4a173ec92b319fdd61d001
lancelee2885 <lancelee2885@gmail.com>
2021-07-23 16:11:04 -0700

    ajax like/unlike in progress

--- static/javascript/warbler.js
+++ static/javascript/warbler.js
@@ -4,15 +4,18 @@ const BASE_URL = "http://localhost:5000";
 async function likeClickHandler(evt){
     evt.preventDefault();
     let messageId = $(evt.target).attr("id");
+    let csrfToken = $("#csrf_token")
     
     const response = await axios({
       url: `${BASE_URL}/messages/${messageId}/like`,
       method: "POST",
+      params: 
     });
     
     console.log(response);
-    
+
+    return false
 }
 
 
-$(".container").on("submit", "i", likeClickHandler);
+$(".container").on("submit", ".like-btn-form", likeClickHandler);
