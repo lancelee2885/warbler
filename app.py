dcd8449b2069aa271d303d2144f7d1f041a1fee4
magus0 <harvestashya@gmail.com>
2021-07-23 17:58:45 -0700

    heroku database settings2

--- app.py
+++ app.py
@@ -19,8 +19,9 @@ app = Flask(__name__)
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgresql:///warbler'))
+# app.config['SQLALCHEMY_DATABASE_URI'] = (
+    # os.environ.get('DATABASE_URL', 'postgresql:///warbler'))
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
