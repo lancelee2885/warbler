5d27eb65b899516d5a068b87f0df5780e022af53
lancelee2885 <lancelee2885@gmail.com>
2021-07-23 17:04:15 -0700

    still working on ajax

--- static/javascript/warbler.js
+++ static/javascript/warbler.js
@@ -4,13 +4,17 @@ const BASE_URL = "http://localhost:5000";
 async function likeClickHandler(evt){
     evt.preventDefault();
     let messageId = $(evt.target).attr("id");
-    let csrfToken = $("#csrf_token")
+    let csrfToken = $(evt.target).parent.val()
     
     const response = await axios({
       url: `${BASE_URL}/messages/${messageId}/like`,
       method: "POST",
-      params: 
+      data: {
+        "csrf_token": csrfToken
+      }
     });
+
+
     
     console.log(response);
 
@@ -19,3 +23,5 @@ async function likeClickHandler(evt){
 
 
 $(".container").on("submit", ".like-btn-form", likeClickHandler);
+
+//csrf_token: IjliYzM0ODljNTBhZjc3YzczYzlkYWZiNjNkZDg0NGJkYTU2ZjJmYjgi.YPtWvQ.UX0VgYVAT022lymMuGzCbU79zZg
