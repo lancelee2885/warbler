ab0017087efc22fdcdb9ce50099e079b441e5fdd
Lance L <lance@Lances-MacBook-Air.local>
2021-10-02 22:10:17 -0700

    added sphinx github page extension

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,8 @@ author = 'Lance'
 extensions = [
     'sphinx.ext.autodoc', 
     'sphinx.ext.coverage', 
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx.ext.githubpages', 
 ]
 
 # Add any paths that contain templates here, relative to this directory.
